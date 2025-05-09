92bdbec04e26d88d8b6e6404db5b18d9b4a98de4
Francisco <franciscoocontatoo@gmail.com>
2025-05-09 00:03:52 -0300

    Enviando primeiros códigos

--- database/database.go
+++ database/database.go
@@ -1,1 +1,21 @@
-package database
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	// Opening the connection with the database
+	stringConnection := "admIndustrial:sysIndustrial@/industrial?charset=utf8&parseTime=True&loc=Local"
+
+	db, erro := sql.Open("mysql", stringConnection)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Println(db)
+}
